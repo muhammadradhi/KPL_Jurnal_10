aeab4198cab112a2c00698f1c4585abd2cdc247f
muhammadradhi <radhirafi06@gmail.com>
2026-05-09 15:04:35 +0700

    Menambahkan API Game

---
+++ Controllers/GameController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Modul10_103022400055.Model;
+
+namespace Modul10_103022400055.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GameController : ControllerBase
+    {
+        private static List<Game> gameList = new List<Game>
+        {
+            new Game
+            {
+                Id = 1, Nama = "Valorant", Developer = "Riot Games", TahunRilis = 2020, Genre = "FPS", Rating = 8.5, Platform = new List<string> {"PC"}, Mode = new List<string> {"Multiplayer"}, IsOnline = true, Harga = 0
+            },
+            new Game
+            {
+                Id = 2, Nama = "GTA V", Developer = "Riot Rockstar Games", TahunRilis = 2013, Genre = "Open World", Rating = 9.5, Platform = new List<string> {"PC", "PS4", "PS5", "Xbox"}, Mode = new List<string> {"Singleplayer", "Multiplayer"}, IsOnline = true, Harga = 300000
+            },
+            new Game
+            {
+                Id = 3, Nama = "The Witcher 3", Developer = "CD Projekt RED", TahunRilis = 2015, Genre = "RPG", Rating = 9.7, Platform = new List<string> {"RPG"}, Mode = new List<string> {"Singleplayer"}, IsOnline = true, Harga = 250000
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Game>> GetAll()
+        {
+            return Ok(gameList);
+        }
+
+        [HttpGet("{index}")]
+        public ActionResult<Game> GetByIndex(int index)
+        {
+            if (index < 0 || index >= gameList.Count) return NotFound();
+
+            return Ok(gameList[index]);
+        }
+
+        [HttpPost]
+        public ActionResult Post(Game baru)
+        {
+            gameList.Add(baru);
+            return Ok(gameList);
+        }
+
+        [HttpDelete("{index}")]
+        public ActionResult Delete(int index)
+        {
+            if (index < 0 || index >= gameList.Count) return NotFound();
+
+            gameList.RemoveAt(index);
+            return Ok(new { message = "Film berhasil dihapus" });
+        }
+
+        [HttpPut("{id}")]
+        public void Put([FromBody] Game game) { gameList.Add(game); }
+    }
+}
+    
---
+++ Model/Game.cs
@@ -0,0 +1,33 @@
+﻿namespace Modul10_103022400055.Model
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Nama { get; set; }
+        public string Developer { get; set; }
+        public int TahunRilis { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+        public List<string> Platform { get; set; }
+        public List<string> Mode { get; set; }
+        public bool IsOnline { get; set; }
+        public int Harga {  get; set; }
+        public Game()
+        {
+
+        }
+        public Game(int id, string nama, string developer, int tahunRilis, string genre, double rating, List<string> platform, List<string> mode, bool isOnline, int harga)
+        {
+            Id = id;
+            Nama = nama;
+            Developer = developer;
+            TahunRilis = tahunRilis;
+            Genre = genre;
+            Rating = rating;
+            Platform = platform;
+            Mode = mode;
+            IsOnline = isOnline;
+            Harga = harga;
+        }
+    }
+}
